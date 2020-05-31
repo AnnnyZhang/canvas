d55ddbe9acb6c9c7e4fde1a0b532e2e5e5cda139
AmeyZhang <18175008653@163.com>
2020-05-31 14:34:21 +0800

    更改画线逻辑：将起始点至终止点连起来让线条更自然

--- main.js
+++ main.js
@@ -5,19 +5,40 @@ let ctx = canvas.getContext('2d');
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight
 
-ctx.fillStyle = 'tomato';               //填充颜色
+ctx.strokeStyle = 'tomato';              //图形轮廓颜色
+ctx.lineWidth = 15;                      //线条粗细
+ctx.lineCap = "round";                  //设置连线末端样式
 
-let flg = 'false';
-canvas.onmousedown = ()=> {
+
+//标记是否需要继续绘制
+let flg = false;
+//标记线条开始的位置
+let begin ;
+
+let isTouch = ctx.ontouchmove;
+console.log(isTouch);
+
+canvas.onmousedown = (e)=> {
+    //当鼠标点击时，开始画线
     flg = true;
+    begin = [e.clientX, e.clientY];     //记录线条开始画的位置
 }
 canvas.onmousemove = (e)=> {
     if(flg === true){
-        ctx.beginPath();
-        ctx.arc(e.clientX, e.clientY, 16, 0 ,2*Math.PI);
-        ctx.fill();
+        drawLine(begin[0], begin[1], e.clientX, e.clientY);     //调用画线
     }
+    //记录鼠标最后的位置
+    begin = [e.clientX, e.clientY];
 }
 canvas.onmouseup = ()=> {
     flg = false;
 }
+
+//画线动作
+let drawLine = (x1, y1, x2, y2)=>{
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
