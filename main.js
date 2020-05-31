571e26bd4cb77fa1c09d0adff7a1a0bfbeb0c090
AmeyZhang <18175008653@163.com>
2020-05-31 11:40:25 +0800

    实现鼠标点击后画线，重新点击屏幕时停止画线

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 //画线
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight
 
 ctx.fillStyle = 'tomato';               //填充颜色
 
-var flg = 'true';
+let flg = 'false';
 canvas.onclick = ()=> {
-    if(flg){
-        canvas.onmousemove = (e)=> {
-            ctx.fillRect(e.clientX - 8, e.clientY - 8, 16, 16);           //控制矩形盒的x, y, width, height
-        }
+    flg = !flg;
+}
+canvas.onmousemove = (e)=> {
+    if(flg === true){
+        ctx.fillRect(e.clientX - 8, e.clientY - 8, 16, 16);
     }
-    flg = false;
+
 }
