3b2a93742f5624e399221f247d9d0fc40fec31cd
AmeyZhang <18175008653@163.com>
2020-05-31 11:43:18 +0800

    更改逻辑：鼠标按下时拖动鼠标画线，鼠标松开时停止画线

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ canvas.height = document.documentElement.clientHeight
 ctx.fillStyle = 'tomato';               //填充颜色
 
 let flg = 'false';
-canvas.onclick = ()=> {
-    flg = !flg;
+canvas.onmousedown = ()=> {
+    flg = true;
 }
 canvas.onmousemove = (e)=> {
     if(flg === true){
         ctx.fillRect(e.clientX - 8, e.clientY - 8, 16, 16);
     }
-
+}
+canvas.onmouseup = ()=> {
+    flg = false;
 }
